cbe67ababb2e3400c9b7c3c18f0f806ffe5fccdd
Lucas Geitner <lucasgeitner@MacBook-Pro-de-Lucas.local>
2019-06-13 23:02:45 +0200

    testing testing

--- src/frontend/__tests__/helper.test.ts
+++ src/frontend/__tests__/helper.test.ts
@@ -1,5 +1,26 @@
+import Card from '../components/Card';
+import * as React from 'react';
+
+import { Amap } from '../../util/Props';
+import { render } from '@testing-library/react';
+
+let Amap: Amap = {
+	image_secure_url: 'https://files-qvmwphz4h.now.sh/bence-balla-schottner-1316901-unsplash.jpg',
+	title: 'Amap Joinville le pont',
+	description: 'Amap de joinville',
+	id: 'qfldskjlkfqsdjllkfjsdlkjflskjl',
+	paysans: [
+		{
+			email: 'joe.lepaysan@test.com',
+			name: 'Joe'
+		}
+	]
+};
 
 test('basic', async () => {
-    expect(0).toBe(0);
-    console.log("hey")
+	const { getByText } = render(<Card />);
+
+	const title = getByText('Amap Joinville le pont');
+
+	expect(true).toBeTruthy;
 });
--- src/frontend/components/Card.tsx
+++ src/frontend/components/Card.tsx
@@ -1,47 +1,51 @@
-import * as React from "react"
+import * as React from 'react';
 import { Amap } from '../../util/Props';
-import { isEmpty, length, equals } from 'ramda'
+import { isEmpty, length, equals } from 'ramda';
 
 /**
  * Card of AMAP
  * @param param0
  */
-const Card: React.FunctionComponent<Amap> = ({title, description, image_secure_url, paysans = [{}], }: Amap) => {
+const Card: React.FunctionComponent<Amap> = ({ title, description, image_secure_url, paysans = [{}] }: Amap) => {
+	let isPaysan = !isEmpty(paysans);
+	let paysanLenght = length(paysans);
+	let isOnlyOnePaysan = equals(paysanLenght, 1);
+	let pluralized = isOnlyOnePaysan ? '' : 's';
 
-    let isPaysan = !isEmpty(paysans)
-    let paysanLenght = length(paysans)
-    let isOnlyOnePaysan = equals(paysanLenght, 1)
-    let pluralized = (isOnlyOnePaysan) ? '' : 's'
+	// map pour ajouter tout les champs
 
-   // map pour ajouter tout les champs
+	// let food = map(prop("nourritures"))(paysans)
+	// const food = paysans.map((p: any) => p && console.log(p) && !is(p.nourritures) && !isEmpty(p.nourritures) )
+	//  = concat(food)
+	// let concatFood = chain(identity(food))
+	// let isFood = isEmpty(concatFood)
+	// undefined
+	// [], ["", "", ""]
+	// function
+	// console.log(food, { isFood, concatFood })
+	// const moreThen20Characters = gt(20, concatenedFood)
+	// let foodDisplayd = both(isPaysan, )) ? food : food.slice(0, 21) + "..."
 
+	return (
+		<div className="mb-10 mt-10">
+			<div className="">
+				<img className="shadow-md rounded-lg w-full h-64 object-center object-cover" src={image_secure_url} />
+				<div className="bg-white rounded-lg px-4 py-3 shadow-lg -mt-16 relative m-4">
+					<span className="bg-teal-200 text-teal-800 font-medium uppercase tracking-wide text-xs leading-none px-1 inline-block rounded-lg">
+						Active
+					</span>
+					{/* <div className="ml-2 text-xs text-gray-600 inline-block">{concatenedFood} </div> */}
+					<h4 className="mt-1 text-gray-900 font-semibold text-lg">{title}</h4>
+					<p className="mt-1 text-gray-700 font-light text-sm">{description}</p>
+					{isPaysan && (
+						<p className="mt-3 text-gray-700 font-light text-xs">
+							🚜 {paysanLenght} paysan{pluralized}{' '}
+						</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    // let food = map(prop("nourritures"))(paysans)
-    // const food = paysans.map((p: any) => p && console.log(p) && !is(p.nourritures) && !isEmpty(p.nourritures) )
-    //  = concat(food)
-    // let concatFood = chain(identity(food))
-    // let isFood = isEmpty(concatFood)
-    // undefined
-    // [], ["", "", ""]
-    // function
-    // console.log(food, { isFood, concatFood })
-    // const moreThen20Characters = gt(20, concatenedFood)
-    // let foodDisplayd = both(isPaysan, )) ? food : food.slice(0, 21) + "..."
-
-    return (
-        <div className="mb-10 mt-10">
-            <div className="">
-                <img className="shadow-md rounded-lg w-full h-64 object-center object-cover" src={image_secure_url} />
-                <div className="bg-white rounded-lg px-4 py-3 shadow-lg -mt-16 relative m-4">
-                    <span className="bg-teal-200 text-teal-800 font-medium uppercase tracking-wide text-xs leading-none px-1 inline-block rounded-lg">Active</span>
-                    {/* <div className="ml-2 text-xs text-gray-600 inline-block">{concatenedFood} </div> */}
-                    <h4 className="mt-1 text-gray-900 font-semibold text-lg">{title}</h4>
-                    <p className="mt-1 text-gray-700 font-light text-sm">{description}</p>
-                    {isPaysan && <p className="mt-3 text-gray-700 font-light text-xs">🚜 {paysanLenght} paysan{pluralized} </p>}
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Card
+export default Card;
